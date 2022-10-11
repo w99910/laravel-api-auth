6ef8d36960d1306a0187eb3e4419b4efd77bb975
Zaw Lin Tun <zawlintun@utycc.edu.mm>
2022-10-11 18:30:58 +0630

    make data as dynamic in response

--- src/Support/ApiResponse.php
+++ src/Support/ApiResponse.php
@@ -6,12 +6,18 @@ use Zlt\LaravelApiAuth\Enums\Status;
 
 class ApiResponse
 {
-    public function __construct(public readonly string $message, public readonly Status $status, public readonly array $data = [])
+    public function __construct(public readonly string $message, public readonly Status $status, public $data = [])
     {
     }
 
     public function __invoke(): \Illuminate\Http\JsonResponse
     {
-        return response()->json(array_merge(['message' => $this->message, 'data' => $this->data]), $this->status->value);
+        $data = ['message' => $this->message];
+        if (is_array($this->data)) {
+            $data = array_merge($data, $this->data);
+        } else {
+            $data[] = $this->data;
+        }
+        return response()->json($data, $this->status->value);
     }
 }
